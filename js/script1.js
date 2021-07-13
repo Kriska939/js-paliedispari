30cc255498f61cf47ccb16f3add8589b16d4b352
Kriska939 <cristinacorrado93@gmail.com>
2021-07-13 10:10:02 +0200

    added palindrome logic

--- js/script1.js
+++ js/script1.js
@@ -9,14 +9,42 @@ Creare una funzione per capire se la parola inserita è palindroma
 
 var userWord = prompt("Inserisci una parola", "esose");
 
-// Creare una funzione per capire se la parola inserita è palindroma: (letta al contrario ha lo stesso significato)
+// validazione - BONUS
 
-function checkPalindromia(word) {
+while (!userWord || userWord.trim() === "") {
+    userWord = prompt("Inserisci una parola", "esose");
+}
 
-    var characters = userWord.split("");
+userWord = userWord.trim().toLowerCase();
 
+// Creare una funzione per capire se la parola inserita è palindroma: (letta al contrario ha lo stesso significato). "isPalindrome" presuppone, dal nome, che l'esito della funzione dovrà essere true o false.
 
+function isPalindrome(word) {
+
+    //girare al contrario la parola dell'utente
+
+    var reverseWord = "";
+
+    for (i = word.length - 1; i >= 0; i--) {
+
+        var character = word[i];
+        reverseWord += character;
+    }
+
+    // verificare se la parola è uguale all'originale (palindromia)
+
+    if (reverseWord === word) {
+        return true;
+    } else {
+        return false;
+    }
 
 }
 
+var palindromeOrNot = isPalindrome(userWord);
 
+if (palindromeOrNot) {
+    console.log("La parola è palindroma");
+} else {
+    console.log("La parola non è palindroma");
+}
